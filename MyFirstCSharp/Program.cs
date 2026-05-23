2946e9c59ab46444da582f551ee064ac42c506f0
LaplusSUGAR <47522266+LaplusSUGAR@users.noreply.github.com>
2026-05-23 14:09:07 +0000

    파이썬과 비교하여 c#의 특징을 알아보자

--- MyFirstCSharp/Program.cs
+++ MyFirstCSharp/Program.cs
@@ -1,27 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace MyFirstCSharp
+namespace SharpStudy
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // 1. 변수 선언과 초기화
-            string characterName = "Gaurdian";
-            int level = 11;
-            float speed = 4.5f;
-            bool isGrounded = true;
+            // 1. 파이썬과 다른 리스트 사용법 (Generic)
+            List<string> inventory = new List<string>();
+            inventory.Add("단검");
+            inventory.Add("물약");
 
-            // 2. 출력 (데이터 확인)
-            Console.WriteLine("==Character Info==");
-            Console.WriteLine("Name: "+characterName);
-            Console.WriteLine("Level: "+level);
-            Console.WriteLine("Moving Speed: "+speed);
-            Console.WriteLine("Is It Grounded? "+isGrounded);
+            // 2. 엄격한 형변환 (float 뒤에 f를 안 붙이면 에러!)
+            float moveSpeed = 5.5f;
+            int roundedSpeed = (int)moveSpeed; // 소수점 버림
 
-            // 3. 데이터 변경
-            level = level + 1;
-            Console.WriteLine("Congratulation! Level UP to "+level);
+            // 3. 배열 선언 (크기 고정)
+            string[] skills = new string[3] {"베기", "찌르기", "방어"};
+
+            Console.WriteLine($"캐릭터 정보: 속도 {roundedSpeed}, 첫번째 스킬 {skills[0]}");
+
+            // 4. 반복문 (파이썬의 for item in list와 비교)
+            foreach (string item in inventory)
+            {
+                Console.WriteLine($"인벤토리 아이템: {item}");
+            }
         }
     }
 }
