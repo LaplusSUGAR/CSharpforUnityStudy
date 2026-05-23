acd18674748d68bcfac4414c1bb492bbcf4bb546
LaplusSUGAR <47522266+LaplusSUGAR@users.noreply.github.com>
2026-05-23 13:41:14 +0000

    C# 첫 번째 프로젝트 생성 및 출력 연습

---
+++ MyFirstCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 여기에 코드를 작성합니다.
+            string name = "코딩 초보";
+            int level = 1;
+
+            Console.WriteLine("안녕하세요! 유니티 공부를 시작합니다.");
+            Console.WriteLine("나의 이름은: " + name);
+            Console.WriteLine("현재 레벨은: " + level);
+        }
+    }
+}
